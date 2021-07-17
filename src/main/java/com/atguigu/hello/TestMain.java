aa7deadbc3ebdd1a10ccc1935572ba3b3e07408a
dunn <dunn@atguigu.com>
2021-07-17 17:27:15 +0800

    8

--- src/main/java/com/atguigu/hello/TestMain.java
+++ src/main/java/com/atguigu/hello/TestMain.java
@@ -3,6 +3,7 @@ package com.atguigu.hello;
 public class TestMain {
     public static void main(String[] args) {
         System.out.println("计算机大佬");
+        System.out.println("abcdef");
     }
 
     public void testUpdate() {
